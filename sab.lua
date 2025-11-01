be87ec4129b1a808225598b74ac28b142de03cb5
sankvik <staskolaev91@gmail.com>
2025-11-01 10:39:09 +0300

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -1,1 +1,22 @@
 local fold = Instance.new("Folder")
+Instance.new("Folder")
+fold.Parent = game.CoreGui
+fold.Name = "svkFolder"
+--
+local sc = Instance.new("ScreenGui")
+Instance.new("ScreenGui")
+sc.Parent = fold
+sc.Name = "svkSC"
+--
+local fr = Instance.new("Frame")
+Instance.new("Frame")
+fr.Parent = sc
+fr.Name = "svkFrame"
+--
+fr.BackgroundColor3 = Color3.new(0,0,15)
+fr.Size = UDim2.new(0,570,0,391)
+fr.Position = UDim2.new(0.347,0,0.137,0)
+Instance.new("UICorner")
+fr.Draggable = true
+Instance.new("UICorner").Parent = fr
+Instance.new("UICorner").CornerRadius = UDim.new(0,5)
