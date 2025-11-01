24e93d2f48de1d907f4bbd31ed64c30a6cfdacd7
sankvik <staskolaev91@gmail.com>
2025-11-01 10:43:37 +0300

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -13,7 +13,7 @@ Instance.new("Frame")
 fr.Parent = sc
 fr.Name = "svkFrame"
 --
-fr.BackgroundColor3 = Color3.new(0,0,15)
+fr.BackgroundColor3 = Color3.fromRGB(0,0,15)
 fr.Size = UDim2.new(0,570,0,391)
 fr.Position = UDim2.new(0.347,0,0.137,0)
 Instance.new("UICorner")
