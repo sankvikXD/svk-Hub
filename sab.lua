1c81295c470aecf6add968ef91aca0dc4651bf39
sankvik <staskolaev91@gmail.com>
2025-11-01 14:54:39 +0300

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -24,10 +24,10 @@ Instance.new("UICorner").CornerRadius = UDim.new(0,5)
 local stroke = Instance.new("UIStroke")
 Instance.new("UIStroke")
 stroke.Parent = fr
-stroke.Color = Color3.fromRGB(0,96,99)
+stroke.Color = Color3.fromRGB(90,90,90)
 --
 local fr1 = Instance.new("Frame")
-fr1.BackgroundColor3 = Color3.fromRGB(0,96,99)
+fr1.BackgroundColor3 = Color3.fromRGB(90,90,90)
 Instance.new("Frame")
 fr1.Size = UDim2.new(0,2,0,391)
 fr1.Position = UDim2.new(0.351,0,0,0)
@@ -38,7 +38,7 @@ fr2.Parent = fr
 fr2.Size = UDim2.new(0,570,0,2)
 fr2.Position = UDim2.new(0,0,0.128,0)
 fr2.Name = "otdelka"
-fr2.BackgroundColor3 = Color3.fromRGB(0,96,99)
+fr2.BackgroundColor3 = Color3.fromRGB(90,90,90)
 --
 local title = Instance.new("TextLabel")
 Instance.new("TextLabel")
@@ -62,3 +62,51 @@ title2.Size = UDim2.new(0,200,0,50)
 title2.TextColor3 = Color3.fromRGB(255,255,255)
 title2.BackgroundTransparency = 1
 title2.Font = Enum.Font.Michroma
+--
+local esp = Instance.new("TextButton")
+Instance.new("TextButton")
+esp.Name = "esp"
+esp.Parent = fr
+esp.Size = UDim2.new(0,166,0,40)
+esp.Position = UDim2.new(0.032,0,0.327,0)
+esp.BackgroundTransparency = 1
+esp.Font = Enum.Font.Michroma
+esp.TextSize = 20
+esp.Text = "ESP"
+esp.TextColor3 = Color3.fromRGB(255,255,255)
+local st1 = Instance.new("UIStroke")
+Instance.new("UIStroke")
+st1.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+st1.Color = Color3.fromRGB(0,0,15)
+--
+local esp2 = Instance.new("TextButton")
+Instance.new("TextButton")
+esp2.Name = "esp"
+esp2.Parent = fr
+esp2.Size = UDim2.new(0,166,0,40)
+esp2.Position = UDim2.new(0.032,0,0.486,0)
+esp2.BackgroundTransparency = 1
+esp2.Font = Enum.Font.Michroma
+esp2.TextSize = 20
+esp2.Text = "Player"
+esp2.TextColor3 = Color3.fromRGB(255,255,255)
+local st2 = Instance.new("UIStroke")
+Instance.new("UIStroke")
+st2.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+st2.Color = Color3.fromRGB(0,0,15)
+--
+local esp3 = Instance.new("TextButton")
+Instance.new("TextButton")
+esp3.Name = "esp"
+esp3.Parent = fr
+esp3.Size = UDim2.new(0,166,0,40)
+esp3.Position = UDim2.new(0.032,0,0.169,0)
+esp3.BackgroundTransparency = 1
+esp3.Font = Enum.Font.Michroma
+esp3.TextSize = 20
+esp3.Text = "Stealer"
+esp3.TextColor3 = Color3.fromRGB(255,255,255)
+local st3 = Instance.new("UIStroke")
+Instance.new("UIStroke")
+st3.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+st3.Color = Color3.fromRGB(0,0,15)
