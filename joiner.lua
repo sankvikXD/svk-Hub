5f09a474e2adcb01add082ab655a61997e42fd7d
sankvik <staskolaev91@gmail.com>
2025-11-16 20:40:13 +0300

    Update joiner.lua

--- joiner.lua
+++ joiner.lua
@@ -1,5 +1,8 @@
 -- Services
-local players = game:GetService("Players")
 local uis = game:GetService("UserInputService")
 local http = game:GetService("HttpService")
 local rs = game:GetService("RunService")
+-- Locals
+local web = "" -- Paste here URL
+-- GUI
+
