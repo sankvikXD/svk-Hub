390938a87e56a69dfcb14066bf9f3365c077da8d
sankvik <staskolaev91@gmail.com>
2025-11-17 14:17:55 +0300

    Update joiner.lua

--- joiner.lua
+++ joiner.lua
@@ -1,8 +1,15 @@
+-- Made by svk
+-- TY For Using This AutoJoiner!
 -- Services
 local uis = game:GetService("UserInputService")
 local http = game:GetService("HttpService")
 local rs = game:GetService("RunService")
 -- Locals
-local web = "" -- Paste here URL
+local web = "" -- Paste here URL (For developer)
+local fold = Instance.new("Folder")
 -- GUI
-
+Instance.new("Folder")
+fold.Parent = game.CoreGui
+fold.Name = "svkJoiner"
+--
+local mainfr = Instance.new("Frame")
